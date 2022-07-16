d2d8666241f3b552ca1f5830cfccb15a3cef2bf0
Ryu Ishizuka <ryu.ishzk@gmail.com>
2022-07-16 22:37:53 +0900

    背景画像差し替え / Bootstrap導入

--- js/nxpc.js
+++ js/nxpc.js
@@ -2,9 +2,6 @@
 $(window).on('load', function () {
 	$("#loading").delay(2000).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
 	// $("#nxpc_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
-
-	// コンテンツを非表示に
-	$('.content_wrap').hide();
 });
 
 // スクロールイベント
@@ -15,8 +12,6 @@ $(window).scroll(function () {
 // ナビゲーションクリック
 $('nav a[href*="#"]').click(function () {
 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-	$('.content_wrap').fadeIn();
-
 	var pos = $(elmHash).offset().top;  //idの上部の距離を取得
 	$('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
 
