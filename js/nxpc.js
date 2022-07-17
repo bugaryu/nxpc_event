9e2274ac47440a887bfa4447c8c1b16bbaf42fd4
Ryu Ishizuka <ryu.ishzk@gmail.com>
2022-07-17 23:37:26 +0900

    詳細情報の編集、レスポンシブ対応

--- js/nxpc.js
+++ js/nxpc.js
@@ -5,15 +5,15 @@ $(window).on('load', function () {
 });
 
 // スクロールイベント
-$(window).scroll(function () {
-	$('#bgimg').css('filter', 'blur(40px)'); // 背景をブラー
-});
+// $(window).scroll(function () {
+// 	$('#bgimg').css('filter', 'blur(40px)'); // 背景をブラー
+// });
 
 // ナビゲーションクリック
 $('nav a[href*="#"]').click(function () {
 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
 	var pos = $(elmHash).offset().top;  //idの上部の距離を取得
-	$('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	$('body,html').animate({ scrollTop: pos }, 100); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
 
 	return false;
 });
