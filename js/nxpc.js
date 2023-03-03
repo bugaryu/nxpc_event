720385bee4e9d0558581f422f437ef099d7c06f8
bugaryu <ryu.ishzk@gmail.com>
2023-03-03 17:59:45 +0900

    バックグラウンド音声の再生

--- js/nxpc.js
+++ js/nxpc.js
@@ -12,11 +12,24 @@ $(window).on('load', function () {
 // 	$('#bgimg').css('filter', 'blur(40px)'); // 背景をブラー
 // });
 
+let isPlaying = false
+
 // ナビゲーションクリック
 $('nav a[href*="#"]').click(function () {
 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
 	var pos = $(elmHash).offset().top;  //idの上部の距離を取得
 	$('body,html').animate({ scrollTop: pos }, 200); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+
+	// 音声再生と動画のロード
+	if (isPlaying) {
+		$('#bg-sound').get(0).pause();
+	} else {
+		$('#bg-sound').get(0).load();
+		$('.bg-video').each((idx, el) => {
+			el.load();
+		});
+	}
+	isPlaying = !isPlaying;
 
 	return false;
 });
